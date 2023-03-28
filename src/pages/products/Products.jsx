7a3192b5322b6827145021c2c00c48ace58ae4e1
Abdulmajeed98 <abdulmajeedjaafer18@gmail.com>
2023-03-28 16:58:19 +0300

    add: footer to products page

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -4,6 +4,7 @@ import { productsData } from "../../mocks/mocks"
 import { ReactComponent as ShoppingCart } from "../../assets/icons/shopping-cart.svg"
 import AddToCartModal from "../../components/modals/AddToCartModal"
 import { COLORS_OPTIONS } from "../../shared/constants/constants"
+import Footer from "../../components/footer/Footer"
 
 const Products = () => {
     const [selectedProduct, setSelectedProduct] = useState("")
@@ -18,34 +19,37 @@ const Products = () => {
     }
 
     return (
-        <div className="px-sides pt-32 pb-10">
-            <h1 className="mb-6 text-4xl">Products</h1>
-            <div className="grid grid-cols-1 gap-4 text-gallery md:grid-cols-2 xl:grid-cols-3">
-                {productsData.map(({ id, image, name, price, title }) => (
-                    <Card
-                        key={id}
-                        className="h-full"
-                        image={image}
-                        title={title}
-                        name={name}
-                        description={`${price}$`}
-                        buttonIcon={<ShoppingCart />}
-                        id={id}
-                        onButtonClick={onShopClickHandler}
-                    />
-                ))}
+        <>
+            <div className="px-sides pt-32 pb-10">
+                <h1 className="mb-6 text-4xl">Products</h1>
+                <div className="grid grid-cols-1 gap-4 text-gallery md:grid-cols-2 xl:grid-cols-3 ">
+                    {productsData.map(({ id, image, name, price, title }) => (
+                        <Card
+                            key={id}
+                            className="h-full"
+                            image={image}
+                            title={title}
+                            name={name}
+                            description={`${price}$`}
+                            buttonIcon={<ShoppingCart />}
+                            id={id}
+                            onButtonClick={onShopClickHandler}
+                        />
+                    ))}
+                </div>
+                {/* TODO: implement add to Cart  */}
+                <AddToCartModal
+                    open={isDialogOpen}
+                    onClose={onDialogClose}
+                    image={filteredProduct.image}
+                    name={filteredProduct.name}
+                    price={filteredProduct.price}
+                    colors={COLORS_OPTIONS}
+                    onAddToCart={() => console.log("added to cart")}
+                />
             </div>
-            {/* TODO: implement add to Cart  */}
-            <AddToCartModal
-                open={isDialogOpen}
-                onClose={onDialogClose}
-                image={filteredProduct.image}
-                name={filteredProduct.name}
-                price={filteredProduct.price}
-                colors={COLORS_OPTIONS}
-                onAddToCart={() => console.log("added to cart")}
-            />
-        </div>
+            <Footer />
+        </>
     )
 }
 
