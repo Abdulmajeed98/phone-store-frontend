74d529435502b0ce1a77be10af64d9eb5edc35c8
Abdulmajeed98 <abdulmajeedjaafer18@gmail.com>
2023-04-01 16:07:45 +0300

    add: `Cart` page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,32 @@
-import React from "react"
+import Footer from "../../components/footer/Footer"
+import { useCart } from "../../shared/contexts/CartContext"
+import animatedBasket from "../../assets/icons/basket.gif"
+import { Link } from "react-router-dom"
+import routes from "../../routes/routeDefinitions"
 
 const Cart = () => {
-    return <div>Cart</div>
+    const { cart } = useCart()
+
+    return (
+        <div className="flex min-h-screen flex-col">
+            {!cart.length ? (
+                <div className="flex flex-1 items-center justify-center px-sides pt-32 pb-10">
+                    <div className="flex flex-col gap-3">
+                        <h1 className="text-5xl font-medium uppercase">The cart is empty!</h1>
+                        <img src={animatedBasket} alt="animated basket" width={380} height={380} className="mx-auto" />
+                        <Link
+                            to={routes.products.path}
+                            className="flex items-center justify-center gap-2 rounded-md bg-gradient-to-r from-shakespeare to-dull-lavender py-4 px-10 uppercase text-white transition-transform duration-300 hover:scale-[1.05]">
+                            Add some item
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div className="flex-1 px-sides pt-32 pb-10">Cart</div>
+            )}
+            <Footer />
+        </div>
+    )
 }
 
 export default Cart
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -32,6 +32,7 @@ const Routes = () => {
                 <Navbar />
                 <ContactUs />
             </Route>
+
             <Route exact path={routes.cart.path}>
                 <Navbar />
                 <Cart />
